c729a026a73d4e43c7fc967530391aca0e1a4953
mignot <garcia.andreas.1991@gmail.com>
2026-02-04 16:16:53 +0100

    refactor: rename db tables and update User model

--- api/test/tests/integration/uploaded_track/input/method/post/attributes/file/test_location.py
+++ api/test/tests/integration/uploaded_track/input/method/post/attributes/file/test_location.py
@@ -14,4 +14,4 @@ class TestCase(UploadedTrackTestCase):
         assert response.status_code == status.HTTP_201_CREATED
         assert Path(self.saved_object.track_file.file.name) == \
             Path(self.test_user1.lib_path_relative_to_media) / UploadedTrackTestFilename.METADATA_NONE_MP3
-        assert self.test_user1.does_track_filename_exist_in_lib(UploadedTrackTestFilename.METADATA_NONE_MP3)
+        assert self.test_user1.does_track_filename_exist_in_lib(UploadedTrackTestFilename.METADATA_NONE_MP3.value)
