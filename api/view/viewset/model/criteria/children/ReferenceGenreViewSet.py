463a5d8de4ab83a0874a9589308a09e0453b9420
mignot <garcia.andreas.1991@gmail.com>
2026-02-07 19:12:37 +0100

    feat: add ReferenceGenreViewSet to handle unauthenticated user requests and update URLs

---
+++ api/view/viewset/model/criteria/children/ReferenceGenreViewSet.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import AllowAny
+
+from api.model.user.User import User
+from api.view.viewset.model.criteria.children.GenreViewSet import GenreViewSet
+
+
+class ReferenceGenreViewSet(GenreViewSet):
+    permission_classes = [AllowAny]
+
+    def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
+        if not request.user.is_authenticated:
+            request.user = User.objects.get_system_user()
