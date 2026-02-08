265ce59f9bdda5d8b24bae027d1f8528a9c867cf
mignot <garcia.andreas.1991@gmail.com>
2026-02-08 12:59:55 +0100

    test: create base test case for reference endpoints and update imports in existing tests

--- api/test/tests/integration/reference/test_reference_album.py
+++ api/test/tests/integration/reference/test_reference_album.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from rest_framework import status
 
 from api.model.album.Album import Album
-from api.test.tests.integration.reference.base import ReferenceTestCase
+from api.test.tests.integration.reference.reference_test_case import ReferenceTestCase
 from api.view.pagination.PaginatedResponseFields import PaginatedResponseFields
 
 
