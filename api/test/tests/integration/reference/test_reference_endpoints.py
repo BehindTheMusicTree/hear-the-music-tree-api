c2a8d4f5f4cff92b74ec9856ca51c7b5be50ddd7
mignot <garcia.andreas.1991@gmail.com>
2026-02-08 02:07:30 +0100

    docs: update CHANGELOG to reflect recent feature additions and fixes

---
+++ api/test/tests/integration/reference/test_reference_endpoints.py
@@ -0,0 +1,98 @@
+import os
+from uuid import UUID
+
+from django.contrib.auth.hashers import make_password
+from django.db import models
+from django.urls import reverse
+from rest_framework import status
+
+from api.model.album.Album import Album
+from api.model.artist.Artist import Artist
+from api.model.criteria.children.genre.Genre import Genre
+from api.model.criteria.children.tag.Tag import Tag
+from api.model.play.Play import Play
+from api.model.uploaded_track.UploadedTrack import UploadedTrack
+from api.model.user.User import User
+from api.test.utils.AppTestCase import AppTestCase
+from api.view.pagination.PaginatedResponseFields import PaginatedResponseFields
+
+
+SYSTEM_USER_USERNAME = "test_reference_system_user"
+
+
+class ReferenceEndpointTestCase(AppTestCase):
+    def setUp(self):
+        super().setUp()
+        self._logout()
+
+        self._system_user, created = User.objects.get_or_create(
+            username=SYSTEM_USER_USERNAME,
+            defaults={
+                "is_system": True,
+                "is_active": True,
+                "is_staff": False,
+                "is_superuser": False,
+                "email": "system@test.com",
+                "is_test_user": True,
+                "password": make_password(None),
+            },
+        )
+        if created or not self._system_user.password.startswith("!"):
+            self._system_user.set_unusable_password()
+            self._system_user.save(update_fields=["password"])
+        self._original_tmta_username = os.environ.get("TMTA_USERNAME")
+        os.environ["TMTA_USERNAME"] = SYSTEM_USER_USERNAME
+
+    def tearDown(self):
+        if self._original_tmta_username is not None:
+            os.environ["TMTA_USERNAME"] = self._original_tmta_username
+        elif "TMTA_USERNAME" in os.environ:
+            del os.environ["TMTA_USERNAME"]
+        super().tearDown()
+
+    def _assert_all_results_belong_to_tmta(
+        self, response, model_class: type[models.Model], uuid_field: str = "uuid"
+    ) -> None:
+        assert response.status_code == status.HTTP_200_OK
+        data = response.json()
+        results = data.get(PaginatedResponseFields.RESULTS, [])
+        for item in results:
+            obj_uuid = item.get(uuid_field)
+            if obj_uuid is None:
+                continue
+            obj = model_class.objects.get(**{uuid_field: UUID(str(obj_uuid)) if isinstance(obj_uuid, str) else obj_uuid})
+            assert getattr(obj, "user_id") == self._system_user.id
+
+    def test_reference_uploaded_track_list_then_200(self):
+        self.model_fixture_factory._create_uploaded_track(user=self._system_user, title="tmta track")
+        self.model_fixture_factory._create_uploaded_track(user=self.test_user1, title="user1 track")
+        response = self.api_client.get(path=reverse('reference-uploaded-track-list'))
+        self._assert_all_results_belong_to_tmta(response, UploadedTrack)
+
+    def test_reference_artist_list_then_200(self):
+        self.model_fixture_factory.create_artist("tmta_artist", user=self._system_user)
+        self.model_fixture_factory.create_artist("user1_artist", user=self.test_user1)
+        response = self.api_client.get(path=reverse('reference-artist-list'))
+        self._assert_all_results_belong_to_tmta(response, Artist)
+
+    def test_reference_album_list_then_200(self):
+        self.model_fixture_factory.create_album("tmta_album", user=self._system_user)
+        self.model_fixture_factory.create_album("user1_album", user=self.test_user1)
+        response = self.api_client.get(path=reverse('reference-album-list'))
+        self._assert_all_results_belong_to_tmta(response, Album)
+
+    def test_reference_tag_list_then_200(self):
+        self.model_fixture_factory.create_tag("tmta_tag", user=self._system_user)
+        self.model_fixture_factory.create_tag("user1_tag", user=self.test_user1)
+        response = self.api_client.get(path=reverse('reference-tag-list'))
+        self._assert_all_results_belong_to_tmta(response, Tag)
+
+    def test_reference_genre_list_then_200(self):
+        self.model_fixture_factory.create_genre("tmta_genre", user=self._system_user)
+        self.model_fixture_factory.create_genre("user1_genre", user=self.test_user1)
+        response = self.api_client.get(path=reverse('reference-genre-list'))
+        self._assert_all_results_belong_to_tmta(response, Genre)
+
+    def test_reference_play_list_then_200(self):
+        response = self.api_client.get(path=reverse('reference-play-list'))
+        self._assert_all_results_belong_to_tmta(response, Play)
