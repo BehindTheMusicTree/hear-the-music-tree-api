d3b4b0f990be639db80a4b7da4f92a8649be9ee4
mignot <garcia.andreas.1991@gmail.com>
2026-02-08 13:14:06 +0100

    test: add retrieval and destroy test cases for reference artists

--- api/test/tests/integration/reference/test_reference_artist.py
+++ api/test/tests/integration/reference/test_reference_artist.py
@@ -15,3 +15,14 @@ class ReferenceArtistTestCase(ReferenceTestCase):
         self.model_fixture_factory.create_artist("user1_artist", user=self.test_user1)
         response = self.api_client.get(path=reverse('reference-artist-list'))
         self._assert_all_results_belong_to_tmta(response, Artist)
+
+    def test_reference_artist_retrieve_then_200(self):
+        artist = self.model_fixture_factory.create_artist("tmta_artist", user=self._system_user)
+        response = self.api_client.get(path=reverse('reference-artist-detail', kwargs={'pk': artist.uuid}))
+        self._assert_retrieve_result_belongs_to_tmta(response, Artist)
+
+    def test_reference_artist_destroy_then_204(self):
+        artist = self.model_fixture_factory.create_artist("tmta_artist", user=self._system_user)
+        response = self.api_client.delete(path=reverse('reference-artist-detail', kwargs={'pk': artist.uuid}))
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert not Artist.objects.filter(uuid=artist.uuid).exists()
