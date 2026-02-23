64725aa72ddcf1851d7eb5c903b93e7f098373be
mignot <garcia.andreas.1991@gmail.com>
2026-02-23 20:22:54 +0100

    test: add new test for full metadata retrieval and temp directory validation
    - Introduced a new test case to verify that the temporary directory is empty before and after retrieving full audio metadata.
    - Enhanced the existing test suite in test_full.py to ensure proper validation of the audio metadata retrieval process.

--- api/test/tests/integration/audio_metadata/test_full.py
+++ api/test/tests/integration/audio_metadata/test_full.py
@@ -1,3 +1,6 @@
+import os
+
+from api import settings
 from rest_framework import status
 from api.test.tests.integration.audio_metadata.AudioMetadataTestCase import AudioMetadataTestCase
 
@@ -19,3 +22,11 @@ class TestCase(AudioMetadataTestCase):
         response = self._post_get_full_metadata()
         assert response.status_code == status.HTTP_200_OK
         assert response.json() is not None
+
+    def test_full_metadata_ok_then_temp_dir_empty(self):
+        assert os.listdir(settings.FILE_UPLOAD_TEMP_DIR) == []
+
+        response = self._post_get_full_metadata()
+
+        assert response.status_code == status.HTTP_200_OK
+        assert os.listdir(settings.FILE_UPLOAD_TEMP_DIR) == []
