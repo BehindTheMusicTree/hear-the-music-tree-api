3091cbedba890ecdc6a210204cab0b5adca4322b
mignot <garcia.andreas.1991@gmail.com>
2026-02-23 16:05:28 +0100

    refactor: rename get_unified_metadata to get_app_metadata
    - Updated the audio file metadata handling across multiple modules and tests to use get_app_metadata instead of get_unified_metadata for improved clarity and consistency.
    - This change enhances the naming convention to better reflect the functionality of the method.

--- api/test/tests/integration/audio_metadata/test_full.py
+++ api/test/tests/integration/audio_metadata/test_full.py
@@ -1,2 +1,10 @@
-class TestCase(AppTestCase):
+from rest_framework import status
+from api.test.tests.integration.audio_metadata.AudioMetadataTestCase import AudioMetadataTestCase
+from serializer.audio_metadata.Fields import Fields
+
+
+class TestCase(AudioMetadataTestCase):
     def test_ok(self):
+        response = self._post_get_full_metadata()
+        assert response.status_code == status.HTTP_200_OK
+        assert self.result is not None
