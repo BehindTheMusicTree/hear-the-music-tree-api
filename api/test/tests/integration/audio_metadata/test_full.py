96555d15c8803df61b550677fbd08c91748fd421
mignot <garcia.andreas.1991@gmail.com>
2026-03-03 10:48:14 +0100

    test: enhance audio metadata tests with specific audio file cases
    - Renamed the existing test method to better reflect its purpose.
    - Added a new test case for posting a default MP3 file, asserting a 200 OK response.
    - Introduced another test case for posting a specific Queen recording, ensuring it also returns a 200 OK response.
    - Improved test coverage for the audio metadata retrieval functionality.

--- api/test/tests/integration/audio_metadata/test_full.py
+++ api/test/tests/integration/audio_metadata/test_full.py
@@ -3,11 +3,21 @@ import os
 from api import settings
 from rest_framework import status
 from api.test.tests.integration.audio_metadata.AudioMetadataTestCase import AudioMetadataTestCase
+from api.test.utils.uploaded_track.UploadedTrackTestFilename import UploadedTrackTestFilename
 
 
 class TestCase(AudioMetadataTestCase):
-    def test_ok(self):
-        response = self._post_get_full_metadata()
+    def test_post_audio_file_then_200_ok(self):
+        response = self._post_get_full_metadata(
+            test_uploaded_track_filename=UploadedTrackTestFilename.DEFAULT_MP3
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() is not None
+
+    def test_post_queen_recording_then_200_ok(self):
+        response = self._post_get_full_metadata(
+            test_uploaded_track_filename=UploadedTrackTestFilename.RECORDING_QUEEN_25_MATCHES_BUT_ONE_WITH_BEST_DURATION_AND_MOST_FIELDS_AND_MOST_RELEASE_GROUPS_MP3
+        )
         assert response.status_code == status.HTTP_200_OK
         assert response.json() is not None
 
