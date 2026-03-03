5be6131d6c40c58ae44d975dc3046dd6d534cd24
mignot <garcia.andreas.1991@gmail.com>
2026-03-03 11:23:26 +0100

    test: enhance AppTestCase for better temporary directory management
    - Introduced a flag to track if uploads were used in tests, ensuring proper cleanup of the temporary directory.
    - Updated the tearDown method to assert that the temporary directory is empty after tests that involve file uploads.
    - Simplified assertions in the test_full_metadata_ok_then_temp_dir_empty method for clarity.

--- api/test/tests/integration/audio_metadata/test_full.py
+++ api/test/tests/integration/audio_metadata/test_full.py
@@ -34,14 +34,11 @@ class TestCase(AudioMetadataTestCase):
         assert response.json() is not None
 
     def test_full_metadata_ok_then_temp_dir_empty(self):
-        temp_dir = settings.FILE_UPLOAD_TEMP_DIR
-        for name in os.listdir(temp_dir):
-            os.unlink(os.path.join(temp_dir, name))
-        assert os.listdir(temp_dir) == []
+        assert os.listdir(settings.FILE_UPLOAD_TEMP_DIR) == []
 
         response = self._post_get_full_metadata(
             test_uploaded_track_filename=UploadedTrackTestFilename.DEFAULT_MP3
         )
 
         assert response.status_code == status.HTTP_200_OK
-        assert os.listdir(temp_dir) == []
+        assert os.listdir(settings.FILE_UPLOAD_TEMP_DIR) == []
