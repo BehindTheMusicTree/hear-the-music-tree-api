a44cea7b987d92468e973ba2a4a018f6802dc638
mignot <garcia.andreas.1991@gmail.com>
2026-03-13 17:07:55 +0100

    refactor: update test_album_artists.py to use UploadedTrackInputFieldKey
    - Replaced references to UploadedTrackInputFieldKey with its .value attribute in test assertions for consistency with the updated structure.
    - Ensured that all field accesses in the test cases align with the new implementation.

--- api/test/tests/integration/uploaded_track/input/attributes_source/data/album_attributes/test_album_artists.py
+++ api/test/tests/integration/uploaded_track/input/attributes_source/data/album_attributes/test_album_artists.py
@@ -13,8 +13,8 @@ class TestCase(UploadedTrackTestCase):
     def test_value_then_ok(self):
         value = 'outkast'
         data = {
-            UploadedTrackInputFieldKey.ALBUM_NAME: 'albumito',
-            UploadedTrackInputFieldKey.ALBUM_ARTISTS_NAMES_MULTIPART: value
+            UploadedTrackInputFieldKey.ALBUM_NAME.value: 'albumito',
+            UploadedTrackInputFieldKey.ALBUM_ARTISTS_NAMES_MULTIPART.value: value,
         }
         response = self._post_uploaded_track(UploadedTrackTestFilename.METADATA_NONE_MP3, **data)
 
@@ -25,7 +25,10 @@ class TestCase(UploadedTrackTestCase):
         assert artist.name == value
 
     def test_empty_then_none(self):
-        data = {UploadedTrackInputFieldKey.ALBUM_NAME: "albumito", UploadedTrackInputFieldKey.ALBUM_ARTISTS_NAMES_MULTIPART: []}
+        data = {
+            UploadedTrackInputFieldKey.ALBUM_NAME.value: "albumito",
+            UploadedTrackInputFieldKey.ALBUM_ARTISTS_NAMES_MULTIPART.value: [],
+        }
         response = self._post_uploaded_track(UploadedTrackTestFilename.METADATA_NONE_MP3, **data)
 
         assert response.status_code == status.HTTP_201_CREATED
