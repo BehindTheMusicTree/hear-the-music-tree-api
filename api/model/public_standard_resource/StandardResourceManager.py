2a024ca7cceb58077bbf2f7e5fa4040f74666aae
mignot <garcia.andreas.1991@gmail.com>
2026-04-12 16:46:42 +0200

    fix(api): ruff fixes for models and migrations

--- api/model/public_standard_resource/StandardResourceManager.py
+++ api/model/public_standard_resource/StandardResourceManager.py
@@ -8,7 +8,7 @@ from .Fields import Fields
 T = TypeVar("T", bound=PublicStandardResource)
 
 
-class StandardResourceManager(BaseManager, Generic[T]):
+class StandardResourceManager[T: PublicStandardResource](BaseManager):
     model: type[T]
 
     def get_default_ordering(self):
